0f88b843f3528fb7df8f0f46cbc38c82da7af088
risingcurve <107753509+risingcurve@users.noreply.github.com>
2022-12-02 04:06:58 +0900

    [Bronze III] Title: 곱셈, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -0,0 +1,16 @@
+N = int(input())
+M = int(input())
+
+strm = str(M)
+
+ML = list(strm)
+
+a = int(ML[2]) * N
+b = int(ML[1]) * N
+c = int(ML[0]) * N
+d = M * N
+
+print(a)
+print(b)
+print(c)
+print(d)
